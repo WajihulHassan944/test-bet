1a0dc87fae09e92d24e11bbba386b76ed7c6cae1
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2025-04-04 20:58:55 +0500

    two

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.fantasymmadness.com", // Change to your actual domain
+  siteUrl: "https://www.fantasymmadness.com", 
     generateRobotsTxt: true, // Generates a robots.txt file
     sitemapSize: 5000, // (Optional) Limit URLs per sitemap file
   };
